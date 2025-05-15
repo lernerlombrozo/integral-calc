391a2ab07236d404946def567eb7b8de743550ec
David Lerner <god@Davids-MacBook-Pro.local>
2025-05-14 18:24:42 -0700

    seemly automated changes:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.caca.calc"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2"
+        versionCode = 5
+        versionName = "5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
